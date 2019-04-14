23704c64a1f769dec9c9739f068805020185822b
ValdasSorys <47417153+ValdasSorys@users.noreply.github.com>
2019-04-14 20:00:58 +0300

    Login and logout
    User can login(/login) and logout(/logout).

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -88,7 +88,8 @@ class User implements UserInterface
         // guarantee every user at least has ROLE_USER
         $roles[] = ['ROLE_USER'];
 
-        return array_unique($roles);
+        //return array_unique($roles);
+        return array('ROLE_USER');
     }
 
     public function setRoles(array $roles): self
