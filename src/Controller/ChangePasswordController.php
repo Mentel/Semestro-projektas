4afa445c33fa1455f294ee9a5773454916ced324
ValdasSorys <47417153+ValdasSorys@users.noreply.github.com>
2019-05-13 19:36:43 +0300

    Ability to change password
    Route: /changepassword

--- src/Controller/ChangePasswordController.php
+++ src/Controller/ChangePasswordController.php
@@ -4,32 +4,51 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\ChangePasswordFormType;
+use App\Form\Model\ChangePasswordModel;
 use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class ChangePasswordController extends AbstractController
 {
-    //TODO: Check how this works(where does the user name gets set)
     /**
      * @Route("/changepassword", name="app_changepassword")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function changePassword(Request $request, UserPasswordEncoderInterface $passwordEncoder, TokenStorageInterface $tokenStorage): Response
     {
+
+        $this->denyAccessUnlessGranted('ROLE_USER');
         $form = $this->createForm(ChangePasswordFormType::class);
         $form->handleRequest($request);
-        $error = "test";
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
-
+            $data = $form->getData();
+            $user = $tokenStorage->getToken()->getUser();
+            $checkPass = $passwordEncoder->isPasswordValid($user, $data['oldPassword']);
+            if ($checkPass === true)
+            {
+                $new_pwd_encoded = $passwordEncoder->encodePassword($user, $data['newPassword']);
+                $user->setPassword($new_pwd_encoded);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->flush();
+                return $this->redirectToRoute('app_index');
+            }
+            else
+            {
+                return $this->render('security/changepassword.html.twig', [
+                    'changepasswordForm' => $form->createView(),
+                    'error' => 'Neteisingas slaptažodis',
+                ]);
+            }
         }
 
         return $this->render('security/changepassword.html.twig', [
             'changepasswordForm' => $form->createView(),
-            'error' => $error,
+            'error' => '',
         ]);
     }
 }
