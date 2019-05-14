2519488606a2fa67ef716d4a2800d5935695cd06
Matt <gmatas557@gmail.com>
2019-05-14 21:11:27 +0300

    added category to user
    Will be used to save user subscribtions

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -28,9 +28,15 @@ class Category
      */
     private $events;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", inversedBy="categories")
+     */
+    private $user;
+
     public function __construct()
     {
         $this->events = new ArrayCollection();
+        $this->user = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -71,6 +77,32 @@ class Category
     {
         if ($this->events->contains($event)) {
             $this->events->removeElement($event);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUser(): Collection
+    {
+        return $this->user;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->user->contains($user)) {
+            $this->user[] = $user;
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->user->contains($user)) {
+            $this->user->removeElement($user);
         }
 
         return $this;
