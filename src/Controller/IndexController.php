b091dcab4f551d73b232a93e56d510afb2f04d3b
Matt <gmatas557@gmail.com>
2019-05-15 20:01:30 +0300

    Update IndexController.php

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,14 +7,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Event;
+use Symfony\Component\Security\Core\Security;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
      */
-    public function renderIndex()
+    public function renderIndex(Security $security) :Response
     {
+        $user=$security->getUser();
+        if($user->getRoles()===['ROLE_VERIFIEDNT']){
+            return $this->render('registration/confirm.html.twig', [
+                'email' => $user->getUsername()
+            ]);
+        }
+        $this->denyAccessUnlessGranted('ROLE_USER', null, 'User tried to access a page without having ROLE_USER');
         return $this->render('index.html.twig');
     }
 }
