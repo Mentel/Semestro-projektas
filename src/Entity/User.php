3e06b976a73eff78efa56b5e8bcbfe1bd7fb3f0a
ValdasSorys <47417153+ValdasSorys@users.noreply.github.com>
2019-05-15 12:17:21 +0300

    Revert to commit "Merge pull request #5 from Mentel/category-subscribtion"

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -48,14 +48,9 @@ class User implements UserInterface
     private $events;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $verify;
-  /*
      * @ORM\ManyToMany(targetEntity="App\Entity\Category", mappedBy="user")
      */
     private $categories;
-
 
     public function __construct()
     {
@@ -184,17 +179,6 @@ class User implements UserInterface
         return $this;
     }
 
-
-    public function getVerify(): ?string
-    {
-        return $this->verify;
-    }
-
-    public function setVerify(?string $verify): self
-    {
-        $this->verify = $verify;
-    }
-
     /**
      * @return Collection|Category[]
      */
@@ -219,7 +203,6 @@ class User implements UserInterface
             $this->categories->removeElement($category);
             $category->removeUser($this);
         }
-
 
         return $this;
     }
