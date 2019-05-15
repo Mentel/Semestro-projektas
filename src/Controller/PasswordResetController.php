ee6e7fca3e468de94cad54df995c1354cbb8498f
Matt <gmatas557@gmail.com>
2019-05-16 00:25:24 +0300

    Fixed error when reseting

--- src/Controller/PasswordResetController.php
+++ src/Controller/PasswordResetController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\ResetFormType;
-use App\Form\ResetNewFormTypeFormType;
+use App\Form\ResetNewFormType;
 use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
