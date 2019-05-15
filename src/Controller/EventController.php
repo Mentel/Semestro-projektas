aefd48d6a93e4455d671940438fd58cf087849c1
aksuram <47566165+aksuram@users.noreply.github.com>
2019-05-15 19:19:33 +0300

    added price to eventadd form

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -34,6 +34,7 @@ class EventController extends AbstractController
             $event->setName($data['name']);
             $event->setDate($data['date']);
             $event->setAddress($data['address']);
+            $event->setPrice($data['price']);
             $event->setDescription($data['description']);
 
             $entityManager = $this->getDoctrine()->getManager();
