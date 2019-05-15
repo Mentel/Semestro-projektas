5d228a54249bbc9999d26f831070c072f78f9ef9
Matt <gmatas557@gmail.com>
2019-05-15 14:09:39 +0300

    Updated registration
    user now has to confirm email

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -15,7 +15,7 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, \Swift_Mailer $mailer): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -30,14 +30,28 @@ class RegistrationController extends AbstractController
                 )
             );
             $user->setCreatedAt();
-            $user->setRoles(['ROLE_USER']);
-
+            $user->setRoles(['ROLE_VERIFIEDNT']);
+            $s = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 5)), 0, 5);
+            $user->setVerify($s);
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // do anything else you need here, like send an email
+            $message = (new \Swift_Message('Hello Email'))
+                ->setFrom('send@example.com')
+                ->setTo($user->getEmail())
+                ->setBody(
+                    $this->renderView(
+                    // templates/emails/registration.html.twig
+                        'email/registration.html.twig',
+                        ['verCode' => $user->getVerify(),
+                            'id' => $user->getId()]
+                    ),
+                    'text/html'
+                )
+            ;
+            $mailer->send($message);
 
             return $this->redirectToRoute('app_login');
         }
@@ -46,4 +60,21 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
+    /**
+     * @Route("/register/confirm/{id}/{code}", name="Email_Confirmation")
+     */
+    public function Confirm($id, $code)
+    {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $usr = $entityManager->getRepository(User::class)
+            ->find($id);
+
+        if ($code === $usr->getVerify()) {
+            $usr->setRoles(['ROLE_USER']);
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('app_login');
+    }
+
 }
